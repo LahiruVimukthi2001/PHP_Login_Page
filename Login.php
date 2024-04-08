f7abff533e75c99d6c462206e6bebad6d6d962c6
lahiruvimukthi2001 <lvsudasinghe@students.nsbm.ac.lk>
2024-04-08 21:11:12 +0530

    link files

--- Login.php
+++ Login.php
@@ -2,7 +2,10 @@
 <html>
 <html lang="en">
     <head>
-        
+        <title>Login Form</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="" rel="stylesheet" type="text/css">
     </head>
 
 </html>
