84db47af799fbca79b5e1908bfb0b8f7b416011e
lahiruvimukthi2001 <lvsudasinghe@students.nsbm.ac.lk>
2024-05-05 22:02:26 +0530

    add error js

--- Login.js
+++ Login.js
@@ -2,3 +2,4 @@ var username = document.forms['form']['username'];
 var password = document.forms['form']['password'];
 
 var username_error = document.getElementById('username_error');
+var password_error = document.getElementById('password_error');
