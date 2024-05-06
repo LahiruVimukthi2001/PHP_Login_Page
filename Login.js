00e207e9e63d777a28b2ba7cc6b894dcb7dbd891
lahiruvimukthi2001 <lvsudasinghe@students.nsbm.ac.lk>
2024-05-06 20:31:27 +0530

    add validate function

--- Login.js
+++ Login.js
@@ -3,3 +3,13 @@ var password = document.forms['form']['password'];
 
 var username_error = document.getElementById('username_error');
 var password_error = document.getElementById('password_error');
+
+function validated(){
+    if(username.value.length <2)
+    {
+        username.style.border ="1px solid red";
+        username_error.style.display = "bolock";
+        email.focus();
+        return false;
+    }
+}
