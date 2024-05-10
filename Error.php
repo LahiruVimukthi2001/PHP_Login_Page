743d2d129fc798865e4ffd82456f05ee323dd30e
lahiruvimukthi2001 <lvsudasinghe@students.nsbm.ac.lk>
2024-05-10 20:14:33 +0530

    add error.php page

---
+++ Error.php
@@ -0,0 +1,13 @@
+<html lang="en">
+    <head>
+        <title>Login Form</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <head>
+        <title>Error Page</title>
+    </head>
+    <body>
+        <h1>Error !</h1>
+    </body>
+</html>
+
