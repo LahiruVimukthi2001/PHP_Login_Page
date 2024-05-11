5c85b025563bb0b9c4defc17f06a9c9655a289e8
lahiruvimukthi2001 <lvsudasinghe@students.nsbm.ac.lk>
2024-05-11 19:08:36 +0530

    add sucess.php page

---
+++ Success.php
@@ -0,0 +1,12 @@
+<html lang="en">
+    <head>
+        <title>Login Form</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <head>
+        <title>Success</title>
+    </head>
+    <body>
+        <h1>Sucess !</h1>
+    </body>
+</html>
