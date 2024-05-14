ba7b875b26f9b54c31ddd0ffa3c309ab3ff4a05a
lahiruvimukthi2001 <lvsudasinghe@students.nsbm.ac.lk>
2024-05-14 21:32:01 +0530

    add connection

--- Server.php
+++ Server.php
@@ -5,8 +5,28 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
     $username = $_POST['username'];
     $password = $_POST['password'];
 
+    //database connection
+    $severname = "localhost";
+    $db_name = "db01";
+    $con = new mysqli("localhost","root","","db01");
+    $sql="insert into login(username,password) values('$username','$password')";
+    $check = mysqli_query($con,$sql);
 
+    if($con)
+    {
+        echo "connected ";
+    }
+    else{
+        echo "failes ";
+    }
 
+    if($check)
+    {
+        echo "Data sended";
+    }
+    else
+    {
+        echo "Data not sended";
+    }
 }
-
 ?>
